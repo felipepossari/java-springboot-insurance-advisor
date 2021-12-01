4f28a3c107ee7ff715075d9a94cc3d898859e893
Felipe Possari <fcpfelipe@hotmail.com>
2021-12-01 14:22:02 -0300

    Move invalid requests list to another class

--- src/test/java/com/felipepossari/insuranceadvisor/adapter/in/web/risk/v1/v1/RiskRequestValidatorTest.java
+++ src/test/java/com/felipepossari/insuranceadvisor/adapter/in/web/risk/v1/v1/RiskRequestValidatorTest.java
@@ -54,7 +54,7 @@ class RiskRequestValidatorTest {
     }
 
     @ParameterizedTest(name = "{index} {0}")
-    @MethodSource(value = "invalidCustomerRequests")
+    @MethodSource(value = "com.felipepossari.insuranceadvisor.base.request.InvalidCustomerRequestTestBuilder#invalidCustomerRequests")
     void validateShouldErrorsIfRequestIsInValid(String testName, CustomerDataApiRequest request,
                                                 RiskApiErrorReason errorReason) {
         var errors = new BeanPropertyBindingResult(request, "");
@@ -63,108 +63,5 @@ class RiskRequestValidatorTest {
 
         assertTrue(errors.hasErrors());
         assertEquals(errorReason.getCode(), errors.getAllErrors().get(0).getCode());
-    }
-
-    private static Stream<Arguments> invalidCustomerRequests() {
-        return Stream.of(
-                Arguments.arguments("invalidAgeRequest",invalidAgeRequest(), FIELD_AGE_INVALID),
-                Arguments.arguments("invalidNullAgeRequest", invalidNullAgeRequest(), FIELD_AGE_INVALID),
-                Arguments.arguments("invalidDependentsRequest", invalidDependentsRequest(), FIELD_DEPENDENTS_INVALID),
-                Arguments.arguments("invalidNullObjectHouseRequest", invalidNullObjectHouseRequest(), FIELD_HOUSE_EMPTY),
-                Arguments.arguments("invalidEmptyHouseRequest", invalidEmptyHouseRequest(), FIELD_HOUSE_EMPTY),
-                Arguments.arguments("invalidDifferentStatusHouseRequest", invalidDifferentStatusHouseRequest(), FIELD_HOUSE_INVALID),
-                Arguments.arguments("invalidIncomeRequest", invalidIncomeRequest(), FIELD_INCOME_INVALID),
-                Arguments.arguments("invalidEmptyMaritalStatusRequest", invalidEmptyMaritalStatusRequest(), FIELD_MARITAL_STATUS_EMPTY),
-                Arguments.arguments("invalidNullMaritalStatusRequest", invalidNullMaritalStatusRequest(), FIELD_MARITAL_STATUS_EMPTY),
-                Arguments.arguments("invalidDifferentMaritalStatusRequest", invalidDifferentMaritalStatusRequest(), FIELD_MARITAL_STATUS_INVALID),
-                Arguments.arguments("invalidNullObjectRiskQuestionsRequest", invalidNullObjectRiskQuestionsRequest(), FIELD_RISK_QUESTIONS_EMPTY),
-                Arguments.arguments("invalidLengthRiskQuestionsRequest", invalidLengthRiskQuestionsRequest(), FIELD_RISK_QUESTIONS_INVALID_LENGTH),
-                Arguments.arguments("invalidAnswerValueRiskQuestionsRequest", invalidAnswerValueRiskQuestionsRequest(), FIELD_RISK_QUESTIONS_INVALID_VALUE),
-                Arguments.arguments("invalidNullObjectVehicleRequest", invalidNullObjectVehicleRequest(), FIELD_VEHICLE_EMPTY),
-                Arguments.arguments("invalidNullYearVehicleRequest", invalidNullYearVehicleRequest(), FIELD_VEHICLE_INVALID),
-                Arguments.arguments("invalidYearVehicleRequest", invalidYearVehicleRequest(), FIELD_VEHICLE_INVALID),
-                Arguments.arguments("invalidYearBiggerCurrentYearPlusTwoVehicleRequest", invalidYearBiggerCurrentYearPlusTwoVehicleRequest(), FIELD_VEHICLE_INVALID)
-        );
-    }
-
-    private static CustomerDataApiRequest invalidAgeRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().age(-1).build();
-    }
-
-    private static CustomerDataApiRequest invalidNullAgeRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().age(null).build();
-    }
-
-    private static CustomerDataApiRequest invalidDependentsRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().dependants(null).build();
-    }
-
-    private static CustomerDataApiRequest invalidNullObjectHouseRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().house(null).build();
-    }
-
-    private static CustomerDataApiRequest invalidEmptyHouseRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest()
-                .house(CustomerHouseApiRequestTestBuilder.anEmptyHouseStatusRequest().build())
-                .build();
-    }
-
-    private static CustomerDataApiRequest invalidDifferentStatusHouseRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest()
-                .house(CustomerHouseApiRequestTestBuilder.anInvalidHouseStatusRequest().build())
-                .build();
-    }
-
-    private static CustomerDataApiRequest invalidIncomeRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().income(-1).build();
-    }
-
-    private static CustomerDataApiRequest invalidEmptyMaritalStatusRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().maritalStatus("").build();
-    }
-
-    private static CustomerDataApiRequest invalidNullMaritalStatusRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().maritalStatus(null).build();
-    }
-
-    private static CustomerDataApiRequest invalidDifferentMaritalStatusRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest()
-                .maritalStatus(MARITAL_STATUS_INVALID).build();
-    }
-
-    private static CustomerDataApiRequest invalidNullObjectRiskQuestionsRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().riskQuestions(null).build();
-    }
-
-    private static CustomerDataApiRequest invalidLengthRiskQuestionsRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().riskQuestions(new Integer[]{0, 1}).build();
-    }
-
-    private static CustomerDataApiRequest invalidAnswerValueRiskQuestionsRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().riskQuestions(new Integer[]{0, 1, 2}).build();
-    }
-
-    private static CustomerDataApiRequest invalidNullObjectVehicleRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest().vehicle(null).build();
-    }
-
-    private static CustomerDataApiRequest invalidNullYearVehicleRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest()
-                .vehicle(CustomerVehicleApiRequestTestBuilder.aNullVehicleYearRequest().build()).build();
-    }
-
-    private static CustomerDataApiRequest invalidYearVehicleRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest()
-                .vehicle(CustomerVehicleApiRequestTestBuilder.anInvalidVehicleYearRequest().build()).build();
-    }
-
-    private static CustomerDataApiRequest invalidYearBiggerCurrentYearPlusTwoVehicleRequest() {
-        return CustomerDataApiRequestTestBuilder.aCustomerDataRequest()
-                .vehicle(
-                        CustomerVehicleApiRequestTestBuilder
-                                .aVehicleRequest()
-                                .year(LocalDateTime.now().getYear() + 2)
-                                .build())
-                .build();
     }
 }
