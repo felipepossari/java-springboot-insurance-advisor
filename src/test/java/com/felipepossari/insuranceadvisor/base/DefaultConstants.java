06dc2c07269e120c3b8a1b11c33cdf6589ec8cff
Felipe Possari <fcpfelipe@hotmail.com>
2021-12-01 17:41:55 -0300

    Update constants

--- src/test/java/com/felipepossari/insuranceadvisor/base/DefaultConstants.java
+++ src/test/java/com/felipepossari/insuranceadvisor/base/DefaultConstants.java
@@ -10,11 +10,11 @@ public class DefaultConstants {
     public static final String HOUSE_OWNERSHIP_STATUS_OWNED = "owned";
     public static final String HOUSE_OWNERSHIP_STATUS_MORTGAGED = "mortgaged";
     public static final String HOUSE_OWNERSHIP_STATUS_INVALID = "rented";
-    public static final Integer INCOME = 0;
+    public static final Integer INCOME = 200000;
     public static final String MARITAL_STATUS_MARRIED = "married";
     public static final String MARITAL_STATUS_SINGLE = "single";
     public static final String MARITAL_STATUS_INVALID = "divorced";
-    public static final Integer[] RISK_QUESTIONS = new Integer[]{0, 1, 0};
+    public static final Integer[] RISK_QUESTIONS = new Integer[]{1, 1, 1};
     public static final Integer VEHICLE_YEAR = 2018;
     public static final Integer VEHICLE_YEAR_INVALID = -1;
 }
