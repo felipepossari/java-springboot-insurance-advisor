e089c1624f5ba13071e07cbb2f9bfddaf797dbf2
Felipe Possari <fcpfelipe@hotmail.com>
2021-12-01 13:26:41 -0300

    Add controller advice

---
+++ src/main/java/com/felipepossari/insuranceadvisor/adapter/in/web/risk/v1/exception/RiskApiControllerAdvice.java
@@ -0,0 +1,50 @@
+package com.felipepossari.insuranceadvisor.adapter.in.web.risk.v1.exception;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.felipepossari.insuranceadvisor.adapter.in.web.risk.v1.exception.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.felipepossari.insuranceadvisor.adapter.in.web.risk.v1.exception.RiskApiErrorReason.REQUEST_BODY_PARSE_FAIL;
+
+@ControllerAdvice
+public class RiskApiControllerAdvice {
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<List<ErrorResponse>> handleInvalidRequest(InvalidRequestException ex, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(buildErrorsResponse(ex.getBindingResult().getAllErrors()));
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<List<ErrorResponse>> handleInvalidFormat(InvalidFormatException ex, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(List.of(
+                        ErrorResponse.builder()
+                                .code(REQUEST_BODY_PARSE_FAIL.getCode())
+                                .message(REQUEST_BODY_PARSE_FAIL.getMessage())
+                                .build())
+                );
+    }
+
+    private List<ErrorResponse> buildErrorsResponse(List<ObjectError> allErrors) {
+        return allErrors.stream().map(this::buildErrorResponse)
+                .collect(Collectors.toList());
+    }
+
+    private ErrorResponse buildErrorResponse(ObjectError error) {
+        return ErrorResponse.builder()
+                .code(error.getCode())
+                .message(error.getDefaultMessage())
+                .build();
+    }
+}
