91b3b76e9c1baff9bd8d165ba02d2394acd5056d
Felipe Possari <fcpfelipe@hotmail.com>
2021-12-01 18:26:55 -0300

    Business logic improved

--- src/main/java/com/felipepossari/insuranceadvisor/application/service/CalculateRiskProfileUseCaseService.java
+++ src/main/java/com/felipepossari/insuranceadvisor/application/service/CalculateRiskProfileUseCaseService.java
@@ -4,26 +4,40 @@ import com.felipepossari.insuranceadvisor.application.domain.customer.Customer;
 import com.felipepossari.insuranceadvisor.application.domain.insurance.Insurance;
 import com.felipepossari.insuranceadvisor.application.domain.insurance.InsuranceType;
 import com.felipepossari.insuranceadvisor.application.domain.rule.IncomeVehicleHouseEligibilityRule;
+import com.felipepossari.insuranceadvisor.application.domain.rule.Rule;
 import com.felipepossari.insuranceadvisor.application.port.in.CalculateRiskProfileUseCase;
 import org.springframework.stereotype.Service;
 
 import java.util.EnumMap;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Service
 public class CalculateRiskProfileUseCaseService implements CalculateRiskProfileUseCase {
     @Override
     public EnumMap<InsuranceType, Insurance> calculate(Customer customer) {
+        var insurances = fillInsurancesScore(customer);
+        var rules = buildRuleList();
+        applyRules(customer, insurances, rules);
+        return insurances;
+    }
 
+    private void applyRules(Customer customer, EnumMap<InsuranceType, Insurance> insurances, List<Rule> rules) {
+        rules.forEach(rule -> rule.apply(customer, insurances));
+    }
+
+    private List<Rule>  buildRuleList() {
+        return List.of(
+                new IncomeVehicleHouseEligibilityRule()
+        );
+    }
+
+    private EnumMap<InsuranceType, Insurance> fillInsurancesScore(Customer customer) {
         EnumMap<InsuranceType, Insurance> insurances = new EnumMap<>(InsuranceType.class);
 
         Stream.of(InsuranceType.values()).forEach(i ->
                 insurances.put(i, new Insurance(i, customer.getBaseScore()))
         );
-
-        IncomeVehicleHouseEligibilityRule rule = new IncomeVehicleHouseEligibilityRule();
-        rule.apply(customer, insurances);
-
         return insurances;
     }
 }
