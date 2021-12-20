93ec5f135a6f78301866d24cf647cb3bcba97b7f
Felipe Possari <fcpfelipe@hotmail.com>
2021-12-20 15:44:52 -0300

    Add new Marital status

--- src/main/java/com/felipepossari/insuranceadvisor/application/domain/customer/MaritalStatus.java
+++ src/main/java/com/felipepossari/insuranceadvisor/application/domain/customer/MaritalStatus.java
@@ -1,5 +1,9 @@
 package com.felipepossari.insuranceadvisor.application.domain.customer;
 
 public enum MaritalStatus {
-    SINGLE, MARRIED
+    SINGLE, MARRIED, DIVORCED, DOMESTIC_PARTNERSHIP;
+
+    public static String parseMaritalStatus(String status){
+        return status.replace(" ", "_").toUpperCase();
+    }
 }
