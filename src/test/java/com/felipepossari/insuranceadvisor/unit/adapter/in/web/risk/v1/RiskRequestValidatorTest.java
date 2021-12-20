48d62ecf3328408ed1a36eb1035eabd45a8f033f
Felipe Possari <fcpfelipe@hotmail.com>
2021-12-20 16:58:46 -0300

    Update unit tests of request validator to validate a list of vehicle and houses

--- src/test/java/com/felipepossari/insuranceadvisor/unit/adapter/in/web/risk/v1/RiskRequestValidatorTest.java
+++ src/test/java/com/felipepossari/insuranceadvisor/unit/adapter/in/web/risk/v1/RiskRequestValidatorTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.validation.BeanPropertyBindingResult;
