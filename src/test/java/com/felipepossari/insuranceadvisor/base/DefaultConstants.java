e037180d12202cc4212f44063591831b2d61d3c5
Felipe Possari <fcpfelipe@hotmail.com>
2021-12-20 15:46:04 -0300

    Update invalid marital status from default constants

--- src/test/java/com/felipepossari/insuranceadvisor/base/DefaultConstants.java
+++ src/test/java/com/felipepossari/insuranceadvisor/base/DefaultConstants.java
@@ -12,7 +12,7 @@ public class DefaultConstants {
     public static final String HOUSE_OWNERSHIP_STATUS_INVALID = "rented";
     public static final Integer INCOME = 200000;
     public static final String MARITAL_STATUS_MARRIED = "married";
-    public static final String MARITAL_STATUS_INVALID = "divorced";
+    public static final String MARITAL_STATUS_INVALID = "open partnership";
     public static final Integer[] RISK_QUESTIONS = new Integer[]{1, 1, 1};
     public static final Integer VEHICLE_YEAR = 2018;
     public static final Integer VEHICLE_YEAR_INVALID = -1;
